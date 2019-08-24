f9f73a5c9f7eda5f59c9d9864dfd79d4a60cd364
Matthew Smith-Burlage <mattburlage@gmail.com>
2019-08-24 13:30:13 -0400

    stuff

---
+++ src/components/Gameboard/Gameboard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Gameboard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Gameboard;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
+
+export default class Example extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" light expand="md">
+                    <NavbarBrand href="/">NertzBoard</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/">Current Game</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/">Statistics</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Options
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        New Game
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Switch Room
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
